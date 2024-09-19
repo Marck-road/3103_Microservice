3a4ebea9258bce9d9c76c31a2b31ec823f5f0922
Mark David Calzada <markdavidcalzada@gmail.com>
2024-09-19 21:38:05 +0800

    Added product functionalities

--- products.js
+++ products.js
@@ -4,12 +4,103 @@ const port = 3001;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-});
-
-
+let products = {};
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+    console.log(`It's alive on http://localhost:${port}`);
+});
+
+/*-----------------------------------------------
+    Gets all products with the ff format:
+    http://localhost:3001/products/all
+------------------------------------------------*/
+
+app.get('/products/all', (req, res) => {
+    if(!products || Object.keys(products).length == 0){
+        return res.status(404).json({error: "No product found!"});
+    }
+
+    res.status(200).send({
+        message: 'List of all products',
+        products: products
+    });
+});
+
+/*-----------------------------------------------
+    Gets a product using its ID with the ff format:
+    http://localhost:3001/products/productID
+------------------------------------------------*/
+
+app.get('/products/:productId', (req, res) => {
+    const productId = req.params.productId;
+    const product = products[productId];
+
+    if(!product){
+        return res.status(404).json({error: "Product not found!"});
+    }
+
+    res.json(product);
+});
+
+/*-----------------------------------------------
+    Post a new product with the ff format:
+    http://localhost:3001/products
+------------------------------------------------*/
+
+app.post('/products', (req, res) => {
+    const productData = req.body;    
+    const productId = Object.keys(products).length + 1;
+    products[productId] = productData;
+    const { name: productName } = productData;
+
+
+    // Makes sure we have product in req.body
+    if (!productData){
+        res.status(418).send({
+            message: 'Incomplete product information!'
+        })
+    }
+
+    // If successful, send response of product
+    res.status(201).send({
+        message: `${productName} successfully added with an ID of ${productId}!`,
+        product_name: productName,
+        product_id: productId
+    });
+});
+
+/*-----------------------------------------------
+    Post a new product with the ff format:
+    http://localhost:3001/products/2
+------------------------------------------------*/
+
+app.put('/products/:productId', (req, res) =>{
+    const productNewData = req.body;     
+    const productId = req.params.productId;
+    const product = products[productId];
+
+    if(!product){
+        return res.status(404).json({error: "Product not found!"});
+    }
+
+    products[productId] = productNewData;
+    res.status(200).json({message: "Product updated successfully."});
+
+});
+
+/*-----------------------------------------------
+    Deletes a product using its ID with the ff format:
+    http://localhost:3001/products/productID
+------------------------------------------------*/
+
+app.delete('/products/:productId', (req,res ) =>{
+    const productId = req.params.productId;
+    const product = products[productId];
+
+    if(!product){
+        return res.status(404).json({error: "Product not found!"});
+    }
+
+    delete products[productId];
+    res.status(200).json({message: "Product deleted successfully."});
 });
